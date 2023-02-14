5546088d6db7be93f64dddbfa47ba11d2f48c2fb
Simon Grigenas <grigenas@outlook.com>
2023-02-14 11:56:16 -0700

    Added Categories Directory and Category Item Components
    - Created CategoriesDirectory and CategoryItem components with associated stylesheets.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// Components Import
+import CategoriesDirectory from './components/categories-directory/CategoriesDirectory';
+
+const App = () => {
+
+  return (
+    <div className="app">
+      <CategoriesDirectory />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/categories-directory/CategoriesDirectory.jsx
@@ -0,0 +1,44 @@
+import './CategoriesDirectory.styles.scss';
+import CategoryItem from '../category-item/CategoryItem';
+
+const CategoriesDirectory = () => {
+    const categories = [
+    {
+      "id": 1,
+      "title": "Hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "Womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "Mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ];
+  
+    return (
+    <div className="categories-container">
+        {categories.map((category, i) => {
+        return (
+            <CategoryItem key={category + i} category={category}/>
+        )
+        })}
+    </div>
+    )
+};
+
+export default CategoriesDirectory;
