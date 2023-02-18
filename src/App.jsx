01d0b72df87698ad2dc01e2bc2c48be25eb65c2b
Simon Grigenas <grigenas@outlook.com>
2023-02-18 16:05:37 -0700

    React Router Navigation Implemented
    - Created the Navigation component and various routes for home and shop pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-// Components Import
-import CategoriesDirectory from './components/categories-directory/CategoriesDirectory';
+import Home from "./components/routes/home/Home";
+import Navigation from "./components/routes/navigation/Navigation";
+import { Routes, Route } from 'react-router-dom';
+
+const Shop = () => {
+  return (
+    <h1>I am the shop page.</h1>
+  )
+}
 
 const App = () => {
 
   return (
-    <div className="app">
-      <CategoriesDirectory />
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} /> 
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/routes/navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as Logo } from '../../../assets/sg-logo.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+        <div className="navigation">
+            <div className="nav-links-container">
+                <Link className="logo-container" to='/'>
+                    <Logo className='logo' style={{
+                        maxWidth: '5rem'
+                    }}/>
+                </Link>
+                <Link className="nav-link" to='/shop'>SHOP</Link>
+            </div>
+        </div>
+        <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation;
